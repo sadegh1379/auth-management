c167cc92645afa01ccb33056816a4bba84a40ea7
Sadegh Akbari <akbarisadegh382@gmail.com>
2025-08-10 01:04:27 +0330

    refactor: convert Input component to use forwardRef
    - Updated the Input component to utilize React's forwardRef for better ref handling.
    - Adjusted the input element to accept a ref prop, enhancing its usability in forms.

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, forwardRef } from 'react';
 import styles from './input.module.scss';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -6,28 +6,25 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: string;
 }
 
-const Input: React.FC<InputProps> = ({
-  label,
-  error,
-  id,
-  className,
-  ...props
-}) => {
-  return (
-    <div className={styles.inputWrapper}>
-      {label && (
-        <label htmlFor={id} className={styles.label}>
-          {label}
-        </label>
-      )}
-      <input
-        id={id}
-        className={`${styles.input} ${className ?? ''}`}
-        {...props}
-      />
-      {error && <span className={styles.error}>{error}</span>}
-    </div>
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, id, className, ...props }, ref) => {
+    return (
+      <div className={styles.inputWrapper}>
+        {label && (
+          <label htmlFor={id} className={styles.label}>
+            {label}
+          </label>
+        )}
+        <input
+          id={id}
+          className={`${styles.input} ${className ?? ''}`}
+          ref={ref}
+          {...props}
+        />
+        {error && <span className={styles.error}>{error}</span>}
+      </div>
+    );
+  }
+);
 
 export default Input;
